b15bea006d64e09ce8be943c609702c31c5bdef9
Gary_Hsu <i_always_smile123@yahoo.com.tw>
2021-05-29 19:05:49 +0800

    Add parameter that can control the ration of cornerRadius.
    [what]
    [why]
    [how]
    [risk]
    Auditor:

--- WidgetsLibrary/FDRoundButton.swift
+++ WidgetsLibrary/FDRoundButton.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+@IBDesignable
 class FDRoundButton: UIButton {
 
+    @IBInspectable var radiusRation: CGFloat = 0.0
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = self.bounds.size.height * radiusRation
+    }
 }

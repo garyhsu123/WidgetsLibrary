d1afd45949728c243ddc323e0d02e8ecbff29fee
Gary_Hsu <i_always_smile123@yahoo.com.tw>
2021-05-29 18:30:03 +0800

    First Commit
    [what]
    [why]
    [how]
    [risk]
    Auditor:

---
+++ WidgetsLibrary/FDRoundButton.swift
@@ -0,0 +1,12 @@
+//
+//  FDRoundButton.swift
+//  WidgetsLibrary
+//
+//  Created by Yu-Chun Hsu on 2021/5/29.
+//
+
+import UIKit
+
+class FDRoundButton: UIButton {
+
+}
---
+++ WidgetsLibrary/WidgetsLibrary.h
@@ -0,0 +1,18 @@
+//
+//  WidgetsLibrary.h
+//  WidgetsLibrary
+//
+//  Created by Yu-Chun Hsu on 2021/5/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WidgetsLibrary.
+FOUNDATION_EXPORT double WidgetsLibraryVersionNumber;
+
+//! Project version string for WidgetsLibrary.
+FOUNDATION_EXPORT const unsigned char WidgetsLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WidgetsLibrary/PublicHeader.h>
+
+
